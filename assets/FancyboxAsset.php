824e6c4cf3e6fc2f71cf547b7d59d332509b3836
grozzzny <1234578>
2018-02-19 10:48:32 +0200

    easyii2

--- assets/FancyboxAsset.php
+++ assets/FancyboxAsset.php
@@ -3,13 +3,13 @@ namespace yii\easyii2\assets;
 
 class FancyboxAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@bower/fancybox/source';
+    public $sourcePath = '@grozzzny/depends/fancybox/dist';
 
     public $css = [
-        'jquery.fancybox.css',
+        'jquery.fancybox.min.css',
     ];
     public $js = [
-        'jquery.fancybox.pack.js'
+        'jquery.fancybox.min.js'
     ];
 
     public $depends = ['yii\web\JqueryAsset'];
