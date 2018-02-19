86f3b74967e03eb88b409cf4d707792886ebff98
Dmitry <grozzzny@list.ru>
2018-02-19 21:44:03 +0000

    Text.php edited online with Bitbucket

--- modules/text/models/Text.php
+++ modules/text/models/Text.php
@@ -4,6 +4,15 @@ namespace yii\easyii2\modules\text\models;
 use Yii;
 use yii\easyii2\behaviors\CacheFlush;
 
+
+/**
+ * Class Text
+ * @package yii\easyii2\modules\text\models
+ *
+ * @property int $text_id [int(11)]
+ * @property string $text
+ * @property string $slug [varchar(128)]
+ */
 class Text extends \yii\easyii2\components\ActiveRecord
 {
     const CACHE_KEY = 'easyii2_text';
