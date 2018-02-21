a3b20a04fcf3893a3081e5969216f9d218f0e428
Dmitriy Groznyy <grozzzny@list.ru>
2018-02-21 23:49:40 +0200

    ref

--- migrations/m000000_000000_install.php
+++ migrations/m000000_000000_install.php
@@ -104,47 +104,6 @@ class m000000_000000_install extends \yii\db\Migration
             'status' => Schema::TYPE_BOOLEAN . " DEFAULT '1'"
         ], $this->engine);
 
-        //CATALOG MODULE
-        $this->createTable(catalog\models\Category::tableName(), [
-            'category_id' => $this->primaryKey(),
-            'title' => Schema::TYPE_STRING . '(128) NOT NULL',
-            'image' => Schema::TYPE_STRING . '(128) DEFAULT NULL',
-            'fields' => Schema::TYPE_TEXT . ' NOT NULL',
-            'slug' => Schema::TYPE_STRING . '(128) DEFAULT NULL',
-            'tree' => Schema::TYPE_INTEGER,
-            'lft' => Schema::TYPE_INTEGER,
-            'rgt' => Schema::TYPE_INTEGER,
-            'depth' => Schema::TYPE_INTEGER,
-            'order_num' => Schema::TYPE_INTEGER,
-            'status' => Schema::TYPE_BOOLEAN . " DEFAULT '1'"
-        ], $this->engine);
-        $this->createIndex('slug', catalog\models\Category::tableName(), 'slug', true);
-
-        $this->createTable(catalog\models\Item::tableName(), [
-            'item_id' => $this->primaryKey(),
-            'category_id' => Schema::TYPE_INTEGER,
-            'title' => Schema::TYPE_STRING . '(128) NOT NULL',
-            'description' => Schema::TYPE_TEXT . ' DEFAULT NULL',
-            'available' => Schema::TYPE_INTEGER . " DEFAULT '1'",
-            'price' => Schema::TYPE_FLOAT . " DEFAULT '0'",
-            'discount' => Schema::TYPE_INTEGER . " DEFAULT '0'",
-            'data' => Schema::TYPE_TEXT . ' NOT NULL',
-            'image' => Schema::TYPE_STRING . '(128) DEFAULT NULL',
-            'slug' => Schema::TYPE_STRING . '(128) DEFAULT NULL',
-            'time' => Schema::TYPE_INTEGER .  " DEFAULT '0'",
-            'status' => Schema::TYPE_BOOLEAN . " DEFAULT '1'"
-        ], $this->engine);
-        $this->createIndex('slug', catalog\models\Item::tableName(), 'slug', true);
-
-        $this->createTable(catalog\models\ItemData::tableName(), [
-            'data_id' => $this->primaryKey(),
-            'item_id' => Schema::TYPE_INTEGER,
-            'name' => Schema::TYPE_STRING . '(128) NOT NULL',
-            'value' => Schema::TYPE_STRING . '(1024) DEFAULT NULL',
-        ], $this->engine);
-        $this->createIndex('item_id_name', catalog\models\ItemData::tableName(), ['item_id', 'name']);
-        $this->createIndex('value', catalog\models\ItemData::tableName(), 'value(300)');
-
         //SHOPCART MODULE
         $this->createTable(shopcart\models\Order::tableName(), [
             'order_id' => $this->primaryKey(),
@@ -349,8 +308,6 @@ class m000000_000000_install extends \yii\db\Migration
         $this->dropTable(models\Setting::tableName());
 
         $this->dropTable(Carousel::tableName());
-        $this->dropTable(catalog\models\Category::tableName());
-        $this->dropTable(catalog\models\Item::tableName());
         $this->dropTable(article\models\Category::tableName());
         $this->dropTable(article\models\Item::tableName());
         $this->dropTable(Feedback::tableName());
