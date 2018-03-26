c43a9ff9e130bdc905b8d47df48a68f2e2cc9e06
grozzzny <1234578>
2018-03-26 13:13:58 +0200

    bug photo

--- widgets/Photos.php
+++ widgets/Photos.php
@@ -4,6 +4,7 @@ namespace yii\easyii2\widgets;
 use Yii;
 use yii\base\Widget;
 use yii\base\InvalidConfigException;
+use yii\easyii2\components\ActiveRecord;
 use yii\easyii2\models\Photo;
 
 class Photos extends Widget
@@ -21,7 +22,8 @@ class Photos extends Widget
 
     public function run()
     {
-        $photos = Photo::find()->where(['class' => get_class($this->model), 'item_id' => $this->model->primaryKey])->sort()->all();
+        $modelPhoto = ActiveRecord::getModelByName('Photo', 'admin');
+        $photos = $modelPhoto::find()->where(['class' => get_class($this->model), 'item_id' => $this->model->primaryKey])->sort()->all();
         echo $this->render('photos', [
             'photos' => $photos
         ]);
