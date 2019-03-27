4c5a589efb27b3176dc5ae681929e729777c07ce
Dmitry <grozzzny@list.ru>
2019-03-27 16:42:49 +0200

    Update DateTimePicker.php

--- widgets/DateTimePicker.php
+++ widgets/DateTimePicker.php
@@ -44,6 +44,9 @@ class DateTimePicker extends InputWidget
         if(empty($this->options['locale'])){
             $this->options['locale'] = Data::getLocale();
         }
+        
+        unset($this->options['class']);
+        
         $clientOptions = (count($this->options)) ? Json::encode($this->options) : '';
         $time = $this->model->{$this->attribute} ? $this->model->{$this->attribute} : time();
         $this->getView()->registerJs('
